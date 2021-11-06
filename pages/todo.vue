61167fcd479a83207613e7e61451998fb7550c31
frukus <frukolokoto@gmail.com>
2021-11-06 19:18:26 -0400

    crud Completo

--- pages/todo.vue
+++ pages/todo.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
   div
     h1 hola todo
+    //- //Agregar todo
+    v-form(@submit.prevent="adtd(todo)")
+      v-text-field(v-model="todo.title")
+      v-text-field(v-model="todo.description")
+      v-btn( type="submit"  color="success") ADD-TD
     v-list
       v-subheader OBTENER v-data
       v-item-group
@@ -9,19 +14,48 @@
             v-list-item-title {{ item.title }}
           v-list-item-content
             v-list-item-subtitle {{ item.description }}
-
+          v-list-item-content
+            v-list-item-subtitle {{ item._id }}
+          v-icon(@click.prevent="editItem(item)" color="green" icon).mr-4 mdi-pencil
+          v-icon(@click.prevent="delto(item)" color="red" icon) mdi-delete
+    v-dialog( v-model="dialog")
+      template(v-slot:activator ="{ on, attrs }")
+      v-card
+        v-card-title edit todo
+          v-text-field(v-model="todoEdit.title")
+          v-text-field(v-model="todoEdit.description")
+          v-card-actions
+            v-btn(@click="salvar()" color="primary") guardar
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 export default {
   data: () => ({
-    list: [{ title: "hola", descri: " hola" }, {title : "hola", descri: " hola"}]
+    list: [{ title: "hola", descri: " hola" }, {title : "hola", descri: " hola"}],
+    todo: {},
+    dialog : false,
+    todoEdit: {}
   }),
   computed: {
     ...mapGetters({
       todoos: 'todo/obtodo'
     })
+  },
+  methods: {
+    ...mapActions({
+      adtd: 'todo/addtodo',
+      delto:'todo/deleteTodo',
+      save: 'todo/actu'
+    }),
+    salvar () {
+      this.save(this.todoEdit)
+      this.dialog = false
+    },
+    editItem(item) {
+      this.todoEdit = {...item}
+      this.dialog = true
+    }
   }
 }
 </script>
--- store/todo.js
+++ store/todo.js
@@ -10,11 +10,48 @@ export const getters = {
 export const  mutations = {
   setTodo(state, todoss) {
     state.todos = todoss
+  },
+  todoAdd(state, data) {
+    state.todos.push(data)
+  },
+  delTodo(state, item) {
+    state.todos = state.todos.filter(t => t._id !== item._id)
+  },
+  at(state, item) {
+    const index = state.todos.findIndex(t => t._id === item._id)
+    if(index !== -1){
+      state.todos.splice(index, 1, item)
+    }
   }
 }
 export const actions = {
   async loadTodo({ commit }) {
     const { data } = await this.$axios.get('http://localhost:3000/api')
     commit('setTodo', data)
+  },
+  async addtodo ({ commit }, todo) {
+    try {
+      const { data } = await this.$axios.post('http://localhost:3000/api', todo)
+      console.log(1, data)
+      commit('todoAdd', data)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async deleteTodo({ commit }, item) {
+    try {
+      await this.$axios.delete(`http://localhost:3000/api/${item._id}`)
+      commit('delTodo',item)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async actu({ commit }, item) {
+    try {
+      const res = await this.$axios.put('http://localhost:3000/api/'+ item._id, item)
+      commit('at', res.data)
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
